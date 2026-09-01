6e3f3a26bae70fa9be5701554938e939db384beb
javierddb3-droid <javierddb3@gmail.com>
2026-09-01 12:27:36 -0700

    Change initial size from 30 to 20 in generador.py

--- modules/generador.py
+++ modules/generador.py
@@ -136,7 +136,7 @@ CONFIGURACION_PLANTILLAS = {
             "centro_x": 655,
             "centro_y": 361,
             "ancho_maximo": 430,
-            "tamano_inicial": 30,
+            "tamano_inicial": 20,
             "tamano_minimo": 15,
             "color": "#173D6D",
         },
