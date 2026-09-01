0935925ec5bf265ead18a7b1454c73f2faf2b495
javierddb3-droid <javierddb3@gmail.com>
2026-09-01 12:33:52 -0700

    Update generador.py

--- modules/generador.py
+++ modules/generador.py
@@ -202,7 +202,7 @@ CONFIGURACION_PLANTILLAS = {
         },
         "fecha": {
             "centro_x": 670,
-            "centro_y": 425,
+            "centro_y": 415,
             "ancho_maximo": 470,
             "tamano_inicial": 20,
             "tamano_minimo": 17,
