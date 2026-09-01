6513b728ed9cd480ced138a6b7032492fa401e5b
javierddb3-droid <javierddb3@gmail.com>
2026-09-01 12:26:43 -0700

    Adjust centro_y value in fecha configuration

--- modules/generador.py
+++ modules/generador.py
@@ -142,7 +142,7 @@ CONFIGURACION_PLANTILLAS = {
         },
         "fecha": {
             "centro_x": 655,
-            "centro_y": 430,
+            "centro_y": 410,
             "ancho_maximo": 470,
             "tamano_inicial": 20,
             "tamano_minimo": 17,
